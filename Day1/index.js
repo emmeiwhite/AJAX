b334c41a1b906ccd8d843816f440b61f2c0d021c
emmeiwhite <emmeiwhite@gmail.com>
2018-11-08 19:26:49 +0530

    Day1_Project Complete- We got our response from the server and displayed data'

--- Day1/index.js
+++ Day1/index.js
@@ -1,24 +1,29 @@
 $(document).ready(function(){ // Only when the DOM is loaded into the Browser will jQuery be executed !!!
 
-    const getPosts = ()=>{
+    const $listGroup = $('.list-group');
 
-        const $listGroup = $('.list-group');
+    const getPosts = ()=>{
         $.ajax({
             url:'https://jsonplaceholder.typicode.com/posts',
             method:'GET',
             success:function(data){
                 // ES6 way is to use filter(), by using data.slice(0,10) we will achieve the same goal
-               const tenPosts = data.filter((post)=>{
-                   return post.id<= 10
+               var tenPosts = data.filter((post)=>{
+                   return post.id<=10
                });
 
-               const titles = tenPosts.forEach((post)=>{
-                   $('<li/>',{
+                var listItem = [];
+                tenPosts.forEach((post)=>{
+                    var $lis = $('<li/>',{
                        class:'list-group-item',
                        "title-id":post.id,
                        html:post.title
-                   }).appendTo($listGroup);
-               })
+                   });
+                   listItem.push($lis);
+               });
+
+            //    Performance Optimization : We should go to DOM as fewer times as we can
+               $listGroup.append(listItem);
 
               
             },
@@ -43,8 +48,19 @@ $(document).ready(function(){ // Only when the DOM is loaded into the Browser wi
     // EVENT DELEGATION : It is a concept in which we use a parent element already present in the DOM and set our event on it, with additional
     // parameter in the callback, for dynamic data event to be set... Let's see how we so this !!! 
 
-    $('.list-group').on('click','.list-group-item',function(){ //,Now the event is set on list-group-item
-        alert('List group Item is clicked');
+   $listGroup.on('click','.list-group-item',function(event){ //,Now the event is set on list-group-item
+      const id = $(event.target).attr('title-id');
+      $.ajax({
+          url:'https://jsonplaceholder.typicode.com/posts/'+id,
+          method:'GET',
+          success:function(data){
+              console.log(data);
+              $('.post_content').html(data.body)
+          },
+          error:function(error){
+              console.log(error.message)
+          }
+      })
     })
 
    
