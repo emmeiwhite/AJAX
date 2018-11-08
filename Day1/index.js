6149409c86471aff286fa5c4ebef0b180be6ca9d
emmeiwhite <emmeiwhite@gmail.com>
2018-11-08 18:57:32 +0530

    EVENT DELEGATION

--- Day1/index.js
+++ Day1/index.js
@@ -31,5 +31,21 @@ $(document).ready(function(){ // Only when the DOM is loaded into the Browser wi
 
     getPosts();
 
+    // EVENT DELEGATION : The respond from the server takes some time to be available
+    /*
+        $('.list-group-item').on('click',function(){
+            alert('list-item was clicked');
+        });
+    */
+
+    // The above code wouldn't work! The reason being Dynamic data, that is available in the DOM anytime in future, after API call is served
+
+    // EVENT DELEGATION : It is a concept in which we use a parent element already present in the DOM and set our event on it, with additional
+    // parameter in the callback, for dynamic data event to be set... Let's see how we so this !!! 
+
+    $('.list-group').on('click','.list-group-item',function(){ //,Now the event is set on list-group-item
+        alert('List group Item is clicked');
+    })
+
    
 });
